1de9a47005879fcdd66d40158956209c43adbb73
noone <biblthebest@hotmail.com>
2017-09-21 13:39:27 +0100

    Added solution for problem 5

--- e005/main.c
+++ e005/main.c
@@ -2,101 +2,158 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define WORD_BITS (8 * sizeof(unsigned int))
-
-static inline void bit_set(unsigned int *bitarray, size_t idx) {
-    bitarray[idx / WORD_BITS] |= (1 << (idx % WORD_BITS));
-}
-
-static inline int bit_is_set(unsigned int *bitarray, size_t idx) {
-    return (bitarray[idx / WORD_BITS] & (idx % WORD_BITS)) != 0;
-}
-
-struct induction_set {
-    int n;
-    int *implies;
+typedef struct prime_fac *p_prime_fac;
+struct prime_fac {
+    p_prime_fac next;
+    int num;
+    int count;
 };
 
-struct induction_set *compute_induction_set(int n) {
-    struct induction_set *set = malloc(sizeof(struct induction_set));
-    if(!set) {
+/* look for a node with the given number and return
+ * if if present, else returns nullptr.
+ *
+ * note that, in the context of this application, zero is not
+ * a value valid for num, i.e. 0 is not a factor of any other
+ * number, therefore, if we encounter a node with it's num
+ * attribute set to zero, then we assume this is a newly allocated
+ * node (via calloc or malloc and memset) and so we treat it as a
+ * hit. */
+p_prime_fac search(p_prime_fac list, int num) {
+    for(p_prime_fac ptr=list; ptr; ptr = ptr->next) {
+        if(ptr->num == num || ptr->num == 0) {
+            return ptr;
+        }
+    }
+
+    return NULL;
+}
+
+/* tries to allocate a node in the list, if unsuccessful
+ * it will terminate the program, therefore removing the
+ * need to check the return value. note that the newly allocated
+ * node is zeroed. */
+p_prime_fac alloc_pfac() {
+    p_prime_fac ptr = malloc(sizeof(struct prime_fac));
+
+    if(!ptr) {
         char msg[255];
-        sprintf(msg, "error mallocing induction set for n=%d\n", n);
+        memset(msg, 0, sizeof(msg));
+        sprintf(msg, "could not malloc head struct");
         fputs(msg, stderr);
         exit(1);
     }
+    memset(ptr, 0, sizeof(struct prime_fac));
 
-    set->n = n;
-//    int ncur_set = (n/2)+1;
-//    int *cur_set = calloc(ncur_set, sizeof(int));
-//
-//    if(!cur_set) {
-//        char msg[255];
-//        sprintf(msg, "error callocing cur_set of size %d\n", ncur_set);
-//        fputs(msg, stderr);
-//        exit(1);
-//    }
-//
-//    int cur_set_pos = 0;
+    return ptr;
+}
 
-    printf("bytes: %d\n", n / 8 + 1);
-    unsigned int *set_bits = calloc(n / 8 + 1, sizeof(unsigned int));
-    int nset = 0;
+void free_list(p_prime_fac list) {
+    p_prime_fac n = list;
 
-    /* this could be optimised, but it's intended as pre-initialisation code. */
-    for(int i=n; i >= 1; i--) {
-        /* if it divides fully */
-        if((n%i) == 0) {
-            int other = n/i;
+    while(n) {
+        p_prime_fac next_node = n->next;
+        free(n);
+        n = next_node;
+    }
+}
 
-            if(!bit_is_set(set_bits, i-1)) {
-                nset++;
+/* computes the prime factorisation of a number (intended to be relatively small)
+ * and creates a list which contains them as well as the number of times
+ * they appear in the prime factorisation.
+ *
+ * note: this was taken from the e003 solution. */
+p_prime_fac get_prime_factors(int n) {
+    p_prime_fac head = alloc_pfac();
+    p_prime_fac cur = head;
+
+    long int k = 2;
+
+    while (n > 1 && n >= k) {
+        if ((n % k) == 0) {
+            n /= k;
+
+            p_prime_fac kfac = search(head, k);
+            if(!kfac) {
+                kfac = alloc_pfac();
+                cur->next = kfac;
             }
 
-            /* for a square number, x*x will divide,
-             * we don't want to count it twice. */
-            if(i != other && !bit_is_set(set_bits, other-1)) {
-                nset++;
-            }
-
-            printf("%d %d\n", i, other);
-            /* using zero based indexing in the bitset. */
-            bit_set(set_bits, i-1);
-            bit_set(set_bits, other-1);
-
-            // ignore for now
-            /* theoretically(guestimating) we only need to expand by about n more
-             * elements. the larger n is, the larger the uncertainty on this guess
-             * is. if we underestimate, we will need to expand it again later with
-             * a smaller value of n, reducing the amount of wasted space. */
-//            if(cur_set_pos >= ncur_set) {
-//            }
+            kfac->num = k;
+            kfac->count++;
+            k = 2; /* reset */
+        } else {
+            /* could do += 2 every time after
+             * the initial += 1 for k=2 to skip
+             * half the numbers. */
+            k++;
         }
     }
 
-    set->implies = calloc(sizeof(nset)+1, sizeof(int));
+    return head;
+}
 
-    int j = 0;
-    for(int i=0; i < n; i++) {
-        if(bit_is_set(set_bits, i)) {
-            printf("%d set\n", i);
-            set->implies[j++] = i+1;
+/* goes through the src list and ensures that all of the
+ * prime factors recorded in this list are also represented in dst.
+ *
+ * if the number is not contained in dst, a new node is allocated and
+ * the exponent of that factor is set to the exponent of the same factor in the
+ * src list.
+ *
+ * if the number is present, the exponent must be at least the same as in
+ * the src list. if it is not, it is adjusted. */
+void merge_pfac_lists(p_prime_fac dst, p_prime_fac src) {
+    p_prime_fac dst_tail = dst;
+    while(dst_tail->next) {
+        dst_tail = dst_tail->next;
+    }
+
+    for(p_prime_fac ptr=src; ptr; ptr = ptr->next) {
+        p_prime_fac dst_node;
+
+        if(!(dst_node=search(dst, ptr->num))) {
+            dst_node = alloc_pfac();
+            dst_node->num = ptr->num;
+            dst_node->count = ptr->count;
+            dst_tail->next = dst_node;
+            dst_tail = dst_node;
+            continue;
+        }
+
+        dst_node->num = ptr->num;
+
+        if(dst_node->count < ptr->count) {
+            dst_node->count =  ptr->count;
         }
     }
-
-    printf("set for %d\n", n);
-    int *ptr = set->implies;
-
-    while(--nset > 0) {
-        printf("%d, ", (*ptr) + 1);
-        ptr++;
-    }
-    printf("\n");
-
-    return set;
 }
 
 int main(int argc, char *argv[]) {
-    compute_induction_set(10);
+    /* challenge given */
+    int bound = 20;
+
+    p_prime_fac pfacs = alloc_pfac();
+    p_prime_fac cur_pfacs;
+
+    for(int i=1; i <= bound; i++) {
+        cur_pfacs = get_prime_factors(i);
+        merge_pfac_lists(pfacs, cur_pfacs);
+
+        free_list(cur_pfacs);
+    }
+
+    int res = 1;
+    for (p_prime_fac ptr = pfacs; ptr; ptr = ptr->next) {
+
+        for(int i=0; i < ptr->count; i++) {
+            res *= ptr->num;
+        }
+    }
+
+    printf("res: %d\n", res);
+
+    free_list(pfacs);
+
     return 0;
 }
+
+
